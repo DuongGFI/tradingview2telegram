79255c04f414df916d8f75b980c1b56b300c330b
DuongGFI <139329378+DuongGFI@users.noreply.github.com>
2024-07-19 13:20:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import os
 TOKEN = os.environ.get("TOKEN")
 chat_id = os.environ.get("chat_id")
 
-# class Signal(BaseModel):
-#     mes: str | None = ""
+class Signal(BaseModel):
+    mes: str | None = ""
 app = FastAPI()
 @app.post("/telegram")
-async def telegram(mes: str| None = ""):
-    Url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={mes}"
+async def telegram(signal: Signal):
+    Url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={signal.mes}"
     requests.get(Url) # this sends the message
     print(Url)
     return {"status": "Success"}
